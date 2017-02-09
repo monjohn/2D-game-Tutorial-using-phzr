10cb32ded0bf52d8fb211b25a1731ff61eb8a70c
Monte Johnston <johnston.monte@gmail.com>
2017-02-09 18:03:44 -0500

    Load the assets and print them on screen

--- src/breakout_cljs/core.cljs
+++ src/breakout_cljs/core.cljs
@@ -1,19 +1,25 @@
 (ns breakout-cljs.core
   (:require
     [phzr.game :as pg]
+    [phzr.image :as pi]
+    [phzr.loader :as pl]
+    [phzr.game-object-factory :as pgof]
     [phzr.scale-manager :as sm]
     [phzr.core :as p :refer [pset!]]))
 
 (enable-console-print!)
+
 
 (defn preload [game]
   (let [scale (:scale game)]
     (pset! scale :page-align-horizontally true)
     (pset! scale :page-align-vertically true)
     (pset! scale :scale-mode (sm/const :show-all)))
-  (pset! (:stage game) :background-color "#eee"))
+  (pset! (:stage game) :background-color "#eee")
+  (pl/image (:load game) "ball" "img/ball.png"))
 
-(defn create [game])
+(defn create [game]
+  (let [ball (pgof/sprite (:add game) 50 50 "ball")]))
 
 (defn start-game []
   (pg/->Game 800 600 (p/phaser-constants :auto) "app"
@@ -21,4 +27,5 @@
                   "create"  create}))
 
 (start-game)
+
 (defn on-js-reload [])
