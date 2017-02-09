0cd90f139317ee0cacc187a9aab7e019fe244801
Monte Johnston <johnston.monte@gmail.com>
2017-02-09 15:40:31 -0500

    Scaling

--- src/breakout_cljs/core.cljs
+++ src/breakout_cljs/core.cljs
@@ -1,16 +1,24 @@
 (ns breakout-cljs.core
   (:require
     [phzr.game :as pg]
+    [phzr.scale-manager :as sm]
     [phzr.core :as p :refer [pset!]]))
 
 (enable-console-print!)
 
-(defn create [])
-(defn preload [])
+(defn preload [game]
+  (let [scale (:scale game)]
+    (pset! scale :page-align-horizontally true)
+    (pset! scale :page-align-vertically true)
+    (pset! scale :scale-mode (sm/const :show-all)))
+  (pset! (:stage game) :background-color "#eee"))
+
+(defn create [game])
 
 (defn start-game []
   (pg/->Game 800 600 (p/phaser-constants :auto) "app"
                  {"preload" preload
                   "create"  create}))
 
+(start-game)
 (defn on-js-reload [])
