8d12f86b1a4c48a0382c9693518b2ebc33e3895b
Monte Johnston <johnston.monte@gmail.com>
2017-02-09 22:29:52 -0500

    Physics

--- src/breakout_cljs/core.cljs
+++ src/breakout_cljs/core.cljs
@@ -3,6 +3,7 @@
     [phzr.game :as pg]
     [phzr.image :as pi]
     [phzr.loader :as pl]
+    [phzr.physics :as physics]
     [phzr.game-object-factory :as pgof]
     [phzr.scale-manager :as sm]
     [phzr.core :as p :refer [pset!]]))
@@ -18,21 +19,20 @@
   (pset! (:stage game) :background-color "#eee")
   (pl/image (:load game) "ball" "img/ball.png"))
 
-(def ball-global (atom nil))
 
 (defn create [game]
-  (let [ball (pgof/sprite (:add game) 50 50 "ball")]
-    (reset! ball-global ball)))
-
-(defn update-fn []
-  (pset! @ball-global :x (inc (get @ball-global :x))))
+  (let [ball (pgof/sprite (:add game) 50 50 "ball")
+        arcade (physics/const :arcade)]
+    (physics/start-system (:physics game) arcade)
+    (physics/enable (:physics game) ball arcade)
+    (pset! (get-in ball [:body :velocity])  :y 150)
+    (pset! (get-in ball [:body :velocity])  :x 150)))
 
 
 (defn start-game []
   (pg/->Game 800 600 (p/phaser-constants :auto) "app"
                  {"preload" preload
-                  "create"  create
-                  "update"  update-fn}))
+                  "create"  create}))
 
 (start-game)
 
