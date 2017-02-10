d3a681f8f52a55a259bdc9fddf5a75bd4b5b075d
Monte Johnston <johnston.monte@gmail.com>
2017-02-10 00:10:39 -0500

    Bounce off the walls

--- src/breakout_cljs/core.cljs
+++ src/breakout_cljs/core.cljs
@@ -14,8 +14,8 @@
 (defn preload [game]
   (let [scale (:scale game)]
     (pset! scale :page-align-horizontally true)
-    (pset! scale :page-align-vertically true)
-    (pset! scale :scale-mode (sm/const :show-all)))
+    (pset! scale :page-align-vertically true))
+  ;  (pset! scale :scale-mode (sm/const :show-all)))
   (pset! (:stage game) :background-color "#eee")
   (pl/image (:load game) "ball" "img/ball.png"))
 
@@ -26,11 +26,14 @@
     (physics/start-system (:physics game) arcade)
     (physics/enable (:physics game) ball arcade)
     (pset! (get-in ball [:body :velocity])  :y 150)
-    (pset! (get-in ball [:body :velocity])  :x 150)))
+    (pset! (get-in ball [:body :velocity])  :x 150)
+    (pset! (:body ball) :collide-world-bounds true)
+    (pset! (get-in ball [:body :bounce])  :x 1)
+    (pset! (get-in ball [:body :bounce])  :y 1)))
 
 
 (defn start-game []
-  (pg/->Game 800 600 (p/phaser-constants :auto) "app"
+  (pg/->Game 800 500 (p/phaser-constants :auto) "app"
                  {"preload" preload
                   "create"  create}))
 
